b1256d57658ecde1651d28db1508865a5bbdfce2
Marijn Huizendveld <marijn@huizendveld.com>
2026-07-27 12:49:25 +0200

    :mag: `Executable specification of command handler`
     - `Common specification: Given(1+N) -> When -> Then(1)`

--- src/ExecutableSpecificationOfCommandHandler.spec.ts
+++ src/ExecutableSpecificationOfCommandHandler.spec.ts
@@ -333,4 +333,74 @@ describe("Executable specification of command handler", () => {
                 expect(thens).toStrictEqual(expectedThens);
             });
     });
+
+    test("Common specification: Given(1+N) -> When -> Then(1)", async () => {
+        type AnyTicketingEvent = | TicketsWereOffered | TicketsWereReserved;
+        type AnyTicketingCommand = | OfferTickets | ReserveTickets;
+
+        const ticketsWereOffered: TicketsWereOffered = {
+            _named: "Tickets were offered",
+            ticketSaleId: "ticket-sale:63074afc-3c6d-451e-8eed-ccd2ce03e2c3",
+            ticketSellerId: "ticket-seller:9b079b1c-81b2-4acd-a1b6-75a10c08c595",
+            eventDetails: {
+                show: "Comedytrain",
+                scheduled: ["2026-06-20 20:30", "2026-06-20 22:00", "Europe/Amsterdam"],
+                location: {
+                    venue: "Comedyclub Comedytrain",
+                    address: {
+                        street: "Pazzanistraat",
+                        streetNumber: "1",
+                        streetNumberAddition: "",
+                        postalCode: "1014 DB",
+                        city: "Amsterdam",
+                        country: "NL"
+                    }
+                }
+            },
+            availableTickets: 150,
+            priceInCents: [2250, "EUR"],
+            offeredAt: ["2026-05-04 09:07:15", "Europe/Amsterdam"]
+        };
+
+        const firstTicketsWereReserved: TicketsWereReserved = {
+            _named: "Tickets were reserved",
+            ticketSaleId: "ticket-sale:63074afc-3c6d-451e-8eed-ccd2ce03e2c3",
+            basket: "basket:11111111-1111-1111-1111-111111111111",
+            desiredNumberOfTickets: 2,
+            reservedNumberOfTickets: 2,
+        };
+
+        const reserveTickets: ReserveTickets = {
+            _named: "Reserve tickets!",
+            ticketSaleId: "ticket-sale:63074afc-3c6d-451e-8eed-ccd2ce03e2c3",
+            basket: "basket:11111111-1111-1111-1111-111111111111",
+            desiredNumberOfTickets: 2,
+        };
+
+        const moreTicketsWereReserved: TicketsWereReserved = {
+            _named: "Tickets were reserved",
+            ticketSaleId: "ticket-sale:63074afc-3c6d-451e-8eed-ccd2ce03e2c3",
+            basket: "basket:22222222-2222-2222-2222-222222222222",
+            desiredNumberOfTickets: 6,
+            reservedNumberOfTickets: 6,
+        };
+
+        const preConditions = [ticketsWereOffered, firstTicketsWereReserved];
+        const trigger = reserveTickets;
+        const outcome = moreTicketsWereReserved;
+
+        return (new ExecutableSpecificationOfCommandHandler<AnyTicketingEvent, AnyTicketingCommand>())
+            .given(ticketsWereOffered, firstTicketsWereReserved)
+            .when(trigger)
+            .then(outcome)
+            .execute(async (givens: AnyTicketingEvent[], when: AnyTicketingCommand, thens: AnyTicketingEvent[]) => {
+                const expectedGivens = preConditions;
+                const expectedWhen = trigger;
+                const expectedThens = [outcome];
+
+                expect(givens).toStrictEqual(expectedGivens);
+                expect(when).toStrictEqual(expectedWhen);
+                expect(thens).toStrictEqual(expectedThens);
+            });
+    });
 });
