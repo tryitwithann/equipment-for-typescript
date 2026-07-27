b59fe27045c456177c50acfbc8bf48c8b0a46ec3
Marijn Huizendveld <marijn@huizendveld.com>
2026-07-27 15:06:37 +0200

    :blue_book: Rename parameter.

--- src/ExecutableSpecificationOfCommandHandler.spec.ts
+++ src/ExecutableSpecificationOfCommandHandler.spec.ts
@@ -467,13 +467,13 @@ describe("Executable specification of command handler", () => {
             .given(ticketsWereOffered, firstTicketsWereReserved, someTicketsWereReserved)
             .when(reserveTickets)
             .then(moreTicketsWereReserved, lastTicketsWereReserved)
-            .execute(async (preConditions: AnyTicketingEvent[], when: AnyTicketingCommand, thens: AnyTicketingEvent[]) => {
+            .execute(async (preConditions: AnyTicketingEvent[], trigger: AnyTicketingCommand, thens: AnyTicketingEvent[]) => {
                 const expectedGivens = [ticketsWereOffered, firstTicketsWereReserved, someTicketsWereReserved];
                 const expectedWhen = reserveTickets;
                 const expectedThens = [moreTicketsWereReserved, lastTicketsWereReserved];
 
                 expect(preConditions).toStrictEqual(expectedGivens);
-                expect(when).toStrictEqual(expectedWhen);
+                expect(trigger).toStrictEqual(expectedWhen);
                 expect(thens).toStrictEqual(expectedThens);
             });
     });
