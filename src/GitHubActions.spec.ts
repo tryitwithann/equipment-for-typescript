e6a0717630e9c41faaa17cf229cd61905f1ebab3
Marijn Huizendveld <marijn@huizendveld.com>
2026-07-27 10:51:59 +0200

    :skull: Drop smoke test for continuous integration.

--- src/GitHubActions.spec.ts
+++
@@ -1,7 +0,0 @@
-import {describe, expect, it} from "vitest";
-
-describe('Continuous Integration', () => {
-    it('Should fail to prevent false positives', () => {
-        expect(true).toBe(false)
-    });
-});
