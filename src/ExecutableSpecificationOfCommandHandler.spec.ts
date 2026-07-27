aba9896e421b4bccddb79fda4a43d245f12baa67
Marijn Huizendveld <marijn@huizendveld.com>
2026-07-27 15:00:00 +0200

    :blue_book: Rename variable.

--- src/ExecutableSpecificationOfCommandHandler.spec.ts
+++ src/ExecutableSpecificationOfCommandHandler.spec.ts
@@ -275,11 +275,11 @@ describe("Executable specification of command handler", () => {
             .execute(async (givens: AnyTicketingEvent[], when: AnyTicketingCommand, thens: AnyTicketingEvent[]) => {
                 const expectedPreConditions = [ticketsWereOffered];
                 const expectedTrigger = offerTickets;
-                const expectedThens = [] as const;
+                const expectedOutcomes = [] as const;
 
                 expect(givens).toStrictEqual(expectedPreConditions);
                 expect(when).toStrictEqual(expectedTrigger);
-                expect(thens).toStrictEqual(expectedThens);
+                expect(thens).toStrictEqual(expectedOutcomes);
             });
     });
 
